dafe500e4a4251b771aa7ed339c8d29a8a35f58b
rushi147311 <patilrushikesh3671@gmail.com>
2026-08-06 22:07:10 +0530

    Setup Firebase and Nodemailer

---
+++ client/src/services/firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  // Firebase config nantar paste kar
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
